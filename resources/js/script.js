05050ef52517ee8a8c4ab014eac18387d1696aad
Norrland <79220667+norrland90@users.noreply.github.com>
2023-05-01 08:25:26 +0200

    add theme switcher JS

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,9 +1,13 @@
+// VARIABLES
 const themeSwitcherInput = document.querySelector('.calculator__switch-input');
 const themeSwitcherThumb = document.querySelector('.calculator__switch-thumb');
+const calculator = document.querySelector('.calculator');
 
+// FUNCTIONS
 function getThemeSwitcherValue() {
   const themeSwitcherValue = themeSwitcherInput.value;
   positionSwitcherThumb(themeSwitcherValue);
+  changeTheme(themeSwitcherValue);
 }
 
 function positionSwitcherThumb(themeSwitcherValue) {
@@ -16,4 +20,21 @@ function positionSwitcherThumb(themeSwitcherValue) {
   }
 }
 
+function changeTheme(themeSwitcherValue) {
+  if (themeSwitcherValue === '1') {
+    calculator.classList = 'calculator';
+    document.body.style.backgroundColor = 'var(--clr-bg-main-1)';
+    document.body.style.color = 'var(--clr-txt-light-1)';
+  } else if (themeSwitcherValue === '2') {
+    document.body.style.backgroundColor = 'var(--clr-bg-main-2)';
+    document.body.style.color = 'var(--clr-txt-dark-2)';
+    calculator.classList = 'calculator calculator--theme-2';
+  } else {
+    document.body.style.backgroundColor = 'var(--clr-bg-main-3)';
+    document.body.style.color = 'var(--clr-txt-yellow-3)';
+    calculator.classList = 'calculator calculator--theme-3';
+  }
+}
+
+// EVENT LISTENERS
 themeSwitcherInput.addEventListener('input', getThemeSwitcherValue);
